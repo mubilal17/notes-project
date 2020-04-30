366f0a846d90d0c917810458cdf24c45d3ec0d76
Muhammad Bilal <mubilal17@gmail.com>
2020-04-30 12:46:21 -0400

    joined seperate bundles into 1, to temporarily fix delay issue

--- server/setupBundleScripts.js
+++ server/setupBundleScripts.js
@@ -3,25 +3,15 @@ const babelify = require('babelify');
 const watchify = require('watchify');
 const browserify = require('browserify');
 
-const editorBundle = [
+const bundle = [
+    'sidebar.d/sidebar-link.js',
+    'sidebar.d/sidebar.js',
     'navbar.js',
     'editor.d/editor-elements/document-page.js',
     'editor.d/editor-elements/title.js',
     'editor.d/editor.js',
-
-].map( scriptName => {
-    return './client/React-Components/' + scriptName;
-});
-
-const sidebarScripts = [
-    'sidebar.d/sidebar-link.js',
-    'sidebar.d/sidebar.js',
-].map( scriptName => {
-    return './client/React-Components/' + scriptName;
-});
-
-const pageBundle = [
     'page.js'
+
 ].map( scriptName => {
     return './client/React-Components/' + scriptName;
 });
@@ -45,14 +35,12 @@ function setupBrowserifyBundle(scriptPaths, bundleName)
     bundle.transform(babelify)
         .bundle()
         .on('error', err => console.log('Got Error Bundling ' + bundleName + ': ' + err.message))
-        .on('update', updateBundle);
-
+        .on('update', updateBundle)
+        .pipe(fs.createWriteStream('./client/public/scripts/' + bundleName));
     updateBundle();
 }
 
 module.exports = function(){
 
-    setupBrowserifyBundle(editorBundle, 'editorBundle.js');
-    setupBrowserifyBundle(sidebarScripts, 'sidebarBundle.js');
-    setupBrowserifyBundle(pageBundle, 'pageBundle.js');
+    setupBrowserifyBundle(bundle, 'bundle.js');
 };
