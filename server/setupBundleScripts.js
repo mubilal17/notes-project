6b23886b7266f1e778bd7f8d884db79cb74a060c
Muhammad Bilal <mubilal17@gmail.com>
2020-04-30 16:01:51 -0400

    modify setupBundleScripts.js to only specify entry bundle script

--- server/setupBundleScripts.js
+++ server/setupBundleScripts.js
@@ -3,18 +3,9 @@ const babelify = require('babelify');
 const watchify = require('watchify');
 const browserify = require('browserify');
 
-const bundle = [
-    'sidebar.d/sidebar-link.js',
-    'sidebar.d/sidebar.js',
-    'navbar.js',
-    'editor.d/editor-elements/document-page.js',
-    'editor.d/editor-elements/title.js',
-    'editor.d/editor.js',
-    'page.js'
 
-].map( scriptName => {
-    return './client/React-Components/' + scriptName;
-});
+
+const entryPointReactScripts = './client/React-Components/page.js';
 
 
 
@@ -27,20 +18,19 @@ function setupBrowserifyBundle(scriptPaths, bundleName)
 
     let bundle = browserify(scriptPaths, browserifyOpts);
     let updateBundle =  function() {
+        console.log('Updating ' + bundleName + '.');
         bundle.bundle()
             .on('error', err => console.log('Got Error Bundling ' + bundleName + ': ' + err.message))
             .pipe(fs.createWriteStream('./client/public/scripts/' + bundleName));
     };
 
     bundle.transform(babelify)
-        .bundle()
         .on('error', err => console.log('Got Error Bundling ' + bundleName + ': ' + err.message))
-        .on('update', updateBundle)
-        .pipe(fs.createWriteStream('./client/public/scripts/' + bundleName));
+        .on('update', updateBundle);
+
     updateBundle();
 }
 
 module.exports = function(){
-
-    setupBrowserifyBundle(bundle, 'bundle.js');
+    setupBrowserifyBundle(entryPointReactScripts, 'bundle.js');
 };
