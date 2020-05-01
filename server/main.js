d76b9df34b575e5bcff61742bf43e65f141f2305
Muhammad Bilal <mubilal17@gmail.com>
2020-05-01 19:18:11 -0400

    mend

--- server/main.js
+++
@@ -1,9 +0,0 @@
-var setupBundleScripts = require('./setupBundleScripts');
-var express = require('express');
-var app = express();
-app.use(express.static('client/public'));
-app.use(express.static('build/public'));
-setupBundleScripts();
-app.listen(4040, function () {
-    console.log('listening on http://localhost:4040/');
-});
