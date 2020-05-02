905205f4e9dae3889e2d26c6373f61c6dca83cb3
Muhammad Bilal <mubilal17@gmail.com>
2020-05-01 21:07:09 -0400

    add a temporary repository for page retrieval

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,17 @@
 const setupBundleScripts = require('./setupBundleScripts');
 const express = require('express');
 const app = express();
+import {Repository} from './data/repository'
 
 app.use(express.static('client/public'));
 app.use(express.static('build/public'))
 setupBundleScripts();
+
+
+app.get('/document', function(req, res){
+    let repository = new Repository();
+    res.send(repository.getPage());
+});
 
 app.listen(4040, function(){
     console.log('listening on http://localhost:4040/');
