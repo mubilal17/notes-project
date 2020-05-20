874c53957b0a087c3b00bfcb9819d0c1616af67c
Muhammad Bilal <mubilal17@gmail.com>
2020-05-20 13:37:52 -0400

    Add drag handlers

--- Client/src/Vue Views/EditorViews/PageElement.vue
+++ Client/src/Vue Views/EditorViews/PageElement.vue
@@ -1,27 +1,58 @@
 
 <template>
-    <input class="form-control border-0 my-3" v-model="element.content"
-           placeholder="Content goes here..."
-           v-bind:class="{shadow: showShadow}"
-           v-on:input="onInputChange"
-           v-on:focusin="isFocused = true" v-on:focusout="isFocused = false"
-           v-on:mouseenter="isHovered = true" v-on:mouseleave="isHovered = false" />
+    <form>
+        <div  class="form-row my-3" v-on:dragover="dragOver" v-on:drop="drop">
+            <div class="col align-middle p-auto" draggable="true" v-on:dragstart="drag"  >
+                <span class="btn btn-outline-secondary border-0 material-icons">drag_handle</span>
+            </div>
+            <div class="col-11">
+                <input class="form-control border-0" v-model="element.content"
+                       placeholder="Content goes here..."
+                       v-bind:class="{shadow: showShadow}"
+                       v-on:input="onInputChange"
+                       v-on:focusin="isFocused = true" v-on:focusout="isFocused = false"
+                       v-on:mouseenter="isHovered = true" v-on:mouseleave="isHovered = false" 
+                       />
+            </div>
+        </div>
+    </form>
 </template>
+
+
 
 <script>
     export default {
-        data: function() {
-            return {isFocused: false, isHovered: false};
+        data: function ()
+        {
+            return { isFocused: false, isHovered: false };
         },
         computed: {
-            showShadow: function() {
+            showShadow: function ()
+            {
                 return this.isFocused || this.isHovered;
             }
         },
         props: ['element'],
         methods: {
-            onInputChange: function(event){
+            onInputChange: function (event)
+            {
                 this.$emit('inputModified', event);
+            },
+            dragOver: function (event)
+            {
+
+                event.preventDefault();
+            },
+            drag: function (event)
+            {
+                let element = this.element;
+                console.log('drag start on element.id = ' + element.id);
+                event.dataTransfer.setData('elementId', element.id);
+            },
+            drop: function (event)
+            {
+                let elementId = event.dataTransfer.getData('elementId');
+                console.log('drag dropped from element.id = ' + elementId +  ' onto element.id = ' + this.element.id);
             }
         }
     }
@@ -31,5 +62,4 @@
     input {
         font-size: 1.25em;
     }
-
 </style>
